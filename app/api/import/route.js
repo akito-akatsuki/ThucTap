d7728398b041c4832ea5275f36cd213db77bbd3c
akito-akatsuki <mgniichan@gmail.com>
2026-03-17 02:39:04 +0700

    Big update

--- app/api/import/route.js
+++ app/api/import/route.js
@@ -9,23 +9,53 @@ export async function POST(req) {
       return Response.json({ error: "Missing fields" }, { status: 400 });
     }
 
+    /* =========================
+       GET EMAIL
+    ========================= */
+
     const email = user || "admin";
+
+    /* =========================
+       GET USERNAME FROM DB
+    ========================= */
+
+    let username = email;
+
+    if (email && email !== "admin") {
+      const { data } = await supabase
+        .from("users")
+        .select("username")
+        .eq("email", email)
+        .single();
+
+      if (data?.username) {
+        username = data.username;
+      }
+    }
+
+    /* =========================
+       INSERT LOG
+    ========================= */
 
     const { error } = await supabase.from("stock_movements").insert({
       product_id,
       type: "import",
       quantity,
-      created_by: email,
+      created_by: username,
     });
 
     if (error) {
-      return Response.json({ error: error.message });
+      return Response.json({
+        error: error.message,
+      });
     }
 
     return Response.json({
       success: true,
     });
   } catch (err) {
+    console.log(err);
+
     return Response.json({
       error: "Server error",
     });
