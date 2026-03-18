65a46787cb366d59777f6ecd51ef3ca4b67282aa
akito-akatsuki <mgniichan@gmail.com>
2026-03-18 17:15:50 +0700

    Final product

--- app/auth/callback/page.jsx
+++ app/auth/callback/page.jsx
@@ -4,18 +4,14 @@ import { useEffect } from "react";
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "next/navigation";
 
-export default function AuthCallback() {
+export default function AuthCallbackPage() {
   const router = useRouter();
 
   useEffect(() => {
     const handleAuth = async () => {
       const { data, error } = await supabase.auth.getSessionFromUrl();
-      if (error) {
-        console.error(error.message);
-      } else if (data.session) {
-        // Session đã được set tự động trong Supabase client
-        router.replace("/dashboard"); // redirect sau login
-      }
+      if (error) console.error(error);
+      else if (data.session) router.replace("/dashboard");
     };
     handleAuth();
   }, [router]);
